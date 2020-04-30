0e1865bd718ec5901325159bfd00e39005f041ff
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2020-04-29 23:08:54 -0300

    mock json

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../services/api';
 
 import TitleBar from '../../components/TitleBar';
 
@@ -26,13 +25,32 @@ export default function() {
   }, [query]);
   
   const loadBooks = async () => {
-    const response = await api.get('books', {
-      params: {
-        query
-      }
-    });
-
-    const booksResponse = response.data.items || [];
+    const booksResponse = [
+      {
+        "id": "1YJZGDwAAQBAJ",
+        "title": "React Native - Building Mobile Apps with JavaScript",
+        "authors": "Vladimir Novick",
+        "description": "Your go-to guide to creating truly native iOS and Android mobile applications using React and JavaScript About This Book Build cross-platform best seller native mobile applications in JavaScript with React-Native framework Learn about real world examples like Whatsapp, Instagram or Twitter. Learn all steps in React Native application development workflow from prototyping to deployment Get familiar with various mobile APIs covered in React Native framework and learn how to extend it further to non-supported APIs Who This Book Is For This book is for JavaScript developers who want to learn how to create native mobile apps using React Native.",
+        "thumbnail": "http://books.google.com/books/content?id=YJZGDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=YJZGDwAAQBAJ&source=gbs_api"
+      },
+      {
+        "id": "2YJZGDwAAQBAJ",
+        "title": "React Native - Building Mobile Apps with JavaScript",
+        "authors": "Vladimir Novick",
+        "description": "Your go-to guide to creating truly native iOS and Android mobile applications using React and JavaScript About This Book Build cross-platform best seller native mobile applications in JavaScript with React-Native framework Learn about real world examples like Whatsapp, Instagram or Twitter. Learn all steps in React Native application development workflow from prototyping to deployment Get familiar with various mobile APIs covered in React Native framework and learn how to extend it further to non-supported APIs Who This Book Is For This book is for JavaScript developers who want to learn how to create native mobile apps using React Native.",
+        "thumbnail": "http://books.google.com/books/content?id=YJZGDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=YJZGDwAAQBAJ&source=gbs_api"
+      },
+      {
+        "id": "3YJZGDwAAQBAJ",
+        "title": "React Native - Building Mobile Apps with JavaScript",
+        "authors": "Vladimir Novick",
+        "description": "Your go-to guide to creating truly native iOS and Android mobile applications using React and JavaScript About This Book Build cross-platform best seller native mobile applications in JavaScript with React-Native framework Learn about real world examples like Whatsapp, Instagram or Twitter. Learn all steps in React Native application development workflow from prototyping to deployment Get familiar with various mobile APIs covered in React Native framework and learn how to extend it further to non-supported APIs Who This Book Is For This book is for JavaScript developers who want to learn how to create native mobile apps using React Native.",
+        "thumbnail": "http://books.google.com/books/content?id=YJZGDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=YJZGDwAAQBAJ&source=gbs_api"
+      },
+    ];
 
     setBooks(booksResponse);
     setSize(Math.ceil(Math.random() * 1000));
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/nataliakt/search-books-app'
-});
-
-export default api;
